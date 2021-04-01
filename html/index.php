a5f3d69577b3389833d5fbe515b041269c9f55ae
naoto_hori <naoto@gugenka.jp>
2021-04-01 17:08:22 +0900

    test

--- html/index.php
+++ html/index.php
@@ -1,2 +1,15 @@
 <?php
-echo 'Hello World!!';
+date_default_timezone_set('Asia/Tokyo');
+
+$openTime = new DateTime('2021-02-22 10:00');
+
+//「testsite」
+if (strpos(__DIR__, 'testsite') !== false) {
+    $openTime = $openTime->modify('-1 year');
+}
+
+if (new DateTime < $openTime) : ?>
+    変更前のコード123
+<?php else : ?>
+    変更後のコード321
+<?php endif; ?>
